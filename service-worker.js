e7f40eb6bcca369e2a77f59f52ed34d0fe7f7aa0
classicmediahub <oluwamayowafrancis@gmail.com>
2026-03-13 23:44:43 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //  Full offline support with background sync
 // ═══════════════════════════════════════════════════════
 
-const CACHE_NAME    = 'excellent-day-v10';
+const CACHE_NAME    = 'excellent-day-v11';
 const APP_SHELL     = [
   '/',
   '/index.html',
